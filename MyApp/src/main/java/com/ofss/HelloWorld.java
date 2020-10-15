23430f8f24c3761bfb2ba34a9af45a7ab799ac53
code-luffy <gsayanaid@gmail.com>
2020-10-15 15:02:31 +0530

    third commit

--- MyApp/src/main/java/com/ofss/HelloWorld.java
+++ MyApp/src/main/java/com/ofss/HelloWorld.java
@@ -5,6 +5,8 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World");
+		Address add=new Address(12,"vikas nagar","lucknow");
+		System.out.println(add);
 	}
 
 }
